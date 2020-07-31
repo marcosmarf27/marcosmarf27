e9997184c851add39cc3063b76eca415aaccf3a0
Marcos Casa <marcosmarf27@gmail.com>
2020-07-31 11:55:01 -0300

    testando e-mail

--- app/control/estagios/testes/Teste.class.php
+++ app/control/estagios/testes/Teste.class.php
@@ -7,59 +7,16 @@ class Teste extends TPage
     {
         parent::__construct();
 
-        $baseDate = clone $date = DateTime::createFromFormat('H:i:s' , '00:00:00');
-
-//dados vindo do banco de dados
-$array = [
-    '04:30',
-   
-    
-    '03:40',
-    '02:30'
-];    
-
-//percorre cada valor do array
-foreach($array as $time)
-{
-    //cria-se o date time com o tempo informado
-    $dateTime = DateTime::createFromFormat('H:i' , $time);
-
-    //realiza o diff com a $baseDate para criar o DateInterval
-    $diff = $baseDate->diff($dateTime);
-
-    //adiciona o diff ao DateTime que somará o tempo
-    $date->add($diff);
-}
-
-//realiza o último diff entre o DateTime de soma e a base date
-$interval = $baseDate->diff($date);
-
-echo '<pre>';
-
-       
-print_r($interval);
-
-echo __DIR__;
-print_r($_SERVER);
-echo $_SERVER['HTTP_REFERER'];
-
-$generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-echo $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
-var_dump($generator);
+        $replaces = [];
+        $replaces['nome'] = 'Marcos Antônio';
+        $replaces['parecer'] = 'O aluno deve entregar os PDf do estágio';
+        $html = new THtmlRenderer('app/resources/tutor/email.html');
+        $html->enableSection('main', $replaces);
+        
+        MailService::send( 'marcosmarf27@gmail.com', 'Assunto e-mail teste', $html->getContents(), 'html' );
+        new TMessage('info', _t('Message sent successfully'));
 
 
-  
-  echo '</pre>';
-
-//DateInterval mantêm em dias (%a) tudo que for acima de 24 horas.
-$hours = $interval->format('%H') + ($interval->format('%a') * 24);
-
-//exibe o tempo
-echo $hours.$interval->format(':%I');
-
-$dados = ['10:00', '02:00'];
-
-echo self::somarHoras($dados);
 
 
 // This will output the barcode as HTML output to display in the browser
